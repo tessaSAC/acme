ac491f180213d4e63f448bfe4cb9aa865ff557de
yuyingliu331 <liuyuying90@live.cn>
2016-08-16 19:38:28 -0400

    changes for router

--- routes.js
+++ routes.js
@@ -2,3 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var product = require('./product.model');
+
+router.get('/',function(req, res, next){
+     res.render('index'); 
+})
+
+router.get('/products', function(req,res, next){
+    var products = product.getProducts();
+    res.render('products', {    
+        products: products
+    });
+})
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -6,8 +6,13 @@ var app = express();
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
 
+//app.use(express.static('./views'));
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+var routes = require('./routes');
+
+app.use('/',routes);
+
 app.listen(3000, function(){
   console.log('listening on ' + 3000);
 });
-
-var routes = require('/routes');
